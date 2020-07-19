5e166a77b1b8edc542d084f91dc73e090d6c8b21
ZacharyHinds <67064643+ZacharyHinds@users.noreply.github.com>
2020-07-19 18:39:11 -0400

    Initial development

---
+++ lua/lang/english/vigilante.lua
@@ -0,0 +1,10 @@
+L = LANG.GetLanguageTableReference("english")
+
+-- GENERAL ROLE LANGUAGE STRINGS
+L[VIGILANTE.name] = "Vigilante"
+L["info_popup_" .. VIGILANTE.name] = [[You are a Vigilante! Protect the innocent by any means necessary!]]
+L["body_found_" .. VIGILANTE.abbr] = "They were a Vigilante!"
+L["search_role_" .. VIGILANTE.abbr] = "This person was a Vigilante!"
+L["target_" .. VIGILANTE.name] = "Vigilante"
+L["ttt2_desc_" .. VIGILANTE.name] = [[The Vigilante needs to protect the innocents by any means necessary! You have your special Deagle which will instant kill non-traitors, but has a chance to convert innocents into traitors!]]
+L["credit_" .. VIGILANTE.abbr .. "_all"] = "Vigilante, you have been awarded {num} equipment credit(s) for your performance."
---
+++ lua/terrortown/entities/roles/vigilante/shared.lua
@@ -0,0 +1,46 @@
+
+if SERVER then
+  AddCSLuaFile()
+	resource.AddFile("materials/vgui/ttt/dynamic/roles/icon_vig.vmt")
+end
+
+function ROLE:PreInitialize()
+	self.color = Color(68, 37, 245, 255)
+
+	self.abbr = "vig" -- abbreviation
+	self.surviveBonus = 0 -- bonus multiplier for every survive while another player was killed
+	self.scoreKillsMultiplier = 1 -- multiplier for kill of player of another team
+	self.scoreTeamKillsMultiplier = -8 -- multiplier for teamkill
+	self.unknownTeam = true -- player don't know their teammates
+
+	self.defaultTeam = TEAM_INNOCENT -- the team name: roles with same team name are working together
+	self.defaultEquipment = SPECIAL_EQUIPMENT -- here you can set up your own default equipment
+
+	self.conVarData = {
+		pct = 0.15, -- necessary: percentage of getting this role selected (per player)
+		maximum = 1, -- maximum amount of roles in a round
+		minPlayers = 6, -- minimum amount of players until this role is able to get selected
+		togglable = true, -- option to toggle a role for a client if possible (F1 menu)
+
+    credits = 2,
+    creditsTraitorKill = 1,
+    creditsTraitorDead = 0,
+    random = 40,
+
+    shopFallback = SHOP_FALLBACK_TRAITOR
+	}
+end
+
+function ROLE:Initialize()
+	roles.SetBaseRole(self, ROLE_DETECTIVE)
+end
+
+if SERVER then
+  function ROLE:GiveRoleLoadout(ply, isRoleChange)
+    ply:GiveEquipmentWeapon("weapon_ttt2_vig_deagle")
+  end
+
+  function ROLE:RemoveRoleLoadout(ply, isRoleChange)
+    ply:RemoveEquipmentWeapon("weapon_ttt2_vig_deagle")
+  end
+end
