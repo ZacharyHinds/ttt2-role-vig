24eae2a3200deb290ee93dadb9ffa6d6cdc32ef8
Tekiad <tekiadp@gmail.cm>
2020-07-31 03:31:42 -0300

    Create vigilante.lua
    vigilante spanish translation

---
+++ lua/lang/spanish/vigilante.lua
@@ -0,0 +1,15 @@
+L = LANG.GetLanguageTableReference("spanish")
+
+-- GENERAL ROLE LANGUAGE STRINGS
+L[VIGILANTE.name] = "Vigilante"
+L["info_popup_" .. VIGILANTE.name] = [[¡Eres el Vigilante! ¡Protege a los inocentes a cualquier costo!]]
+L["body_found_" .. VIGILANTE.abbr] = "¡Era un Vigilante!"
+L["search_role_" .. VIGILANTE.abbr] = "Esta persona era un Vigilante."
+L["target_" .. VIGILANTE.name] = "Vigilante"
+L["ttt2_desc_" .. VIGILANTE.name] = [[¡El Vigilante debe proteger a los inocentes bajo cualquier medio! Tu daño incrementa con cada asesinato que hagas a un equipo enemigo, pero recuerda, matar aliados hará que tu daño se reduzca.]]
+L["credit_" .. VIGILANTE.abbr .. "_all"] = "Vigilante, has sido recompensado con {num} créditos por tu desempeño."
+
+L["ttt2_vig_multi"] = "Multiplicador actual: x{multiplier}"
+L["ttt2_vig_enemy_killed"] = "¡Has asesinado a un maleante!"
+L["ttt2_vig_inno_killed"] = "¡Has asesinado a un inocente! ¡Qué mal!"
+L["ttt2_vig_teammate_killed"] = "¡Has asesinado a un compañero! ¡Qué mal!"
