15fa65e8d6f5c3c0c5df6154f8cc0b446fab5d43
Blueberryy <igory.ygr200@gmail.com>
2020-09-27 20:02:55 +0500

    Create vigilante.lua

---
+++ lua/terrortown/lang/russian/vigilante.lua
@@ -0,0 +1,15 @@
+L = LANG.GetLanguageTableReference("Русский")
+
+-- GENERAL ROLE LANGUAGE STRINGS
+L[VIGILANTE.name] = "Линчеватель"
+L["info_popup_" .. VIGILANTE.name] = [[Вы линчеватель! Защитите невиновных любыми средствами!]]
+L["body_found_" .. VIGILANTE.abbr] = "Он был линчевателем!"
+L["search_role_" .. VIGILANTE.abbr] = "Этот человек был линчевателем!"
+L["target_" .. VIGILANTE.name] = "Линчеватель"
+L["ttt2_desc_" .. VIGILANTE.name] = [[Линчевателю нужно защищать невиновных, убивая предателей любыми средствами! Ваш урон увеличивается с каждым убитым вами врагом, но будьте осторожны, если вы убьёте своих союзников, ваш урон уменьшится!]]
+L["credit_" .. VIGILANTE.abbr .. "_all"] = "Vigilante, you have been awarded {num} equipment credit(s) for your performance."
+
+L["ttt2_vig_multi"] = "Текущий множитель: x{multiplier}"
+L["ttt2_vig_enemy_killed"] = "Вы убили врага!"
+L["ttt2_vig_inno_killed"] = "Вы убили невиновного!"
+L["ttt2_vig_teammate_killed"] = "Вы убили товарища по команде!"
